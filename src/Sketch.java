624813ad7db704f2ffaa0ec7194a0855ca648c8a
KaidenChan123 <Kaidenhongli.chan27@ycdsbk12.ca>
2026-06-02 12:39:17 -0400

    Declaring Variables, WIP: Making The Bunny

--- src/Sketch.java
+++ src/Sketch.java
@@ -4,10 +4,19 @@ import processing.core.PApplet;
  * Template for programs with Processing graphics output.
  * @author Your Name
  */
+
+
 public class Sketch extends PApplet {
     public static void main(String[] args) {
         PApplet.main("Sketch");
     }
+
+    // Declaring Variables
+    int x = 50;
+    int y = 50;
+    int bunnySize = 1;
+    String bunnyStage = "Baby";
+
 
     @Override
     public void settings() {
@@ -16,14 +25,33 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
-        background(240, 255, 235);
+        background(135, 206, 235);
 
     }
 
     @Override
     public void draw() {
+        drawBunny();
+    }
+
+
+    public void drawBunny() {
+        // Outer Ears
+        fill(255, 200, 210);
+        stroke(3);
+        ellipse(x + 450, 400, 30, 100);
+        ellipse(x + 550, 400, 30, 100);
+
+
+        // Head
+        fill(255, 255, 255);
+        circle(x + 500, x + 450, 100);
+       
 
     }
+
+
+    
 
     /** Additional helper methods below */
 
