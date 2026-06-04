55018d4ed212e7d28ac158092f1189d5eaa67d06
KaidenChan123 <KaidenHongLi.Chan27@ycdsbk12.ca>
2026-06-04 12:50:25 -0400

    Adding Bunny

--- src/Sketch.java
+++ src/Sketch.java
@@ -31,24 +31,59 @@ public class Sketch extends PApplet {
 
     @Override
     public void draw() {
-        drawBunny();
+        drawBabyBunny();
     }
 
 
-    public void drawBunny() {
+    public void drawBabyBunny() {
         // Outer Ears
         fill(255, 200, 210);
         stroke(3);
-        ellipse(x + 450, 400, 30, 100);
-        ellipse(x + 550, 400, 30, 100);
+        ellipse(x + 490, y + 615, 12, 40);
+        ellipse(x + 510, y + 615, 12, 40);
 
+        // Inner Ears
+        fill(255, 180, 190);
+        noStroke();
+        ellipse(x + 490, y + 615, 6, 26);
+        ellipse(x + 510, y + 615, 6, 26);
 
         // Head
+        stroke(0);
+        strokeWeight(2);
         fill(255, 255, 255);
-        circle(x + 500, x + 450, 100);
+        circle(x + 500, y + 650, 45);
+
+        // Eyes
+        fill(0);
+        noStroke();
+        circle(x + 490, y + 645, 4);
+        circle(x + 510, y + 645, 4);
+
+        // Nose
+        fill(255, 150, 160);
+        circle(x + 500, y + 652, 3);
+
+        // Body
+        fill(255, 255, 255);
+        stroke(0);
+        strokeWeight(2);
+        ellipse(x + 500, y + 685, 55, 50);
+
+
+        // tail
+        fill(255, 255, 255);
+        stroke(0);
+        strokeWeight(2);
+        circle(x + 465, y + 690, 16);
+
        
 
     }
+
+
+
+    
 
 
     
