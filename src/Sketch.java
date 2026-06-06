88c1f69c9d78dec312b016a748fb849f178aa1a5
KaidenChan123 <KaidenHongLi.Chan27@ycdsbk12.ca>
2026-06-06 16:35:00 -0400

    Adding Bunny Growth And Sizing

--- src/Sketch.java
+++ src/Sketch.java
@@ -11,7 +11,7 @@ public class Sketch extends PApplet {
         PApplet.main("Sketch");
     }
 
-    // Declaring Variables
+  // Declaring Variables
     int x = 50;
     int y = 50;
     int clicks = 0;
@@ -24,11 +24,8 @@ public class Sketch extends PApplet {
 
     String bunnyStage = "Baby";
 
-    int[] stageIncrease = {1, 2, 3, 4, 5};
-    int bunnySize = 1;
-    int currentSize = 45;  // Current Size (Baby)
-
-
+    int[] stageIncrease = {1, 2, 3, 4, 5};  // Sizes
+    int currentSize = 45;
 
     @Override
     public void settings() {
@@ -44,120 +41,332 @@ public class Sketch extends PApplet {
     public void draw() {
         background(135, 206, 235);
 
-
-             // Size
         for (int i = 0; i < stageIncrease.length; i++) {
-
+            if (clicks >= adultStage && i == 4) {
+                currentSize = stageIncrease[i];
+            }
         }
 
-        drawBunny();
-
+        drawBabyBunny();
+        drawChildBunny();
+        drawPubertyBunny();
+        drawTeenBunny();
+        drawAdultBunny();
+        
         fill(0);
         textSize(50);
         text("Stage:  " + bunnyStage, 400, 80);
 
         textSize(24);
         text("Clicks:  " + clicks, 50, 50);
-
-
-   
-
-        
-
-    }
-
-
-    public void drawBunny() {
-        // Outer Ears
-        fill(255, 200, 210);
-        stroke(3);
-        ellipse(x + 490, y + 615, 12 * bunnySize, 40 * bunnySize);
-        ellipse(x + 510, y + 615, 12 * bunnySize, 40 * bunnySize);
-
-        // Inner Ears
-        fill(255, 180, 190);
-        noStroke();
-        ellipse(x + 490, y + 615, 6 * bunnySize, 26 * bunnySize);
-        ellipse(x + 510, y + 615, 6 * bunnySize, 26 * bunnySize);
-
-
-        // Body
-        fill(255, 255, 255);
-        stroke(0);
-        strokeWeight(2);
-        ellipse(x + 500, y + 685, 55 * bunnySize, 50 * bunnySize);
-
-        // Head
-        stroke(0);
-        strokeWeight(2);
-        fill(255, 255, 255);
-        circle(x + 500, y + 650, 45 * bunnySize);
-
-        // Eyes
-        fill(0);
-        noStroke();
-        circle(x + 490, y + 645, 4 * bunnySize);
-        circle(x + 510, y + 645, 4 * bunnySize);
-
-        // Nose
-        fill(255, 150, 160);
-        circle(x + 500, y + 652, 3 * bunnySize);
-
-
-
-        // tail
-        fill(255, 255, 255);
-        stroke(0);
-        strokeWeight(2);
-        circle(x + 465, y + 690, 16 * bunnySize);
-
-       
-
     }
 
 
 
+    public void drawBabyBunny() {
+        if (bunnyStage.equals("Baby")) {
+
+            // Tail and body
+            fill(255, 255, 255); 
+            stroke(0);
+            strokeWeight(2);
+            circle(x + 465, y + 690, 16);
+            ellipse(x + 500, y + 685, 55, 50);
+
+            // Ears
+            fill(255, 200, 210);
+            ellipse(x + 490, y + 615, 12, 40);
+            ellipse(x + 510, y + 615, 12, 40);
+
+            // Head
+            fill(255, 255, 255);
+            circle(x + 500, y + 650, 45);
+            
+            // eyes
+            fill(0); 
+            circle(x + 490, y + 645, 4); 
+            circle(x + 510, y + 645, 4);
+            
+            // nose
+            fill(255, 150, 160); 
+            circle(x + 500, y + 652, 3);
+
+            // Pacifier
+            fill(100, 200, 255);
+            circle(x + 500, y + 658, 8);
+            noFill();
+            strokeWeight(1.5f);
+            arc(x + 500, y + 658, 12, 12, 0, PI);
+            strokeWeight(2);
+        }
+    }
+
+
+    
+    public void drawChildBunny() {
+        if (bunnyStage.equals("Child")) {
+            // Tail & Body
+            fill(255, 255, 255); 
+            stroke(0); 
+            strokeWeight(2);
+            circle(x + 450, y + 660, 22);
+            ellipse(x + 500, y + 650, 80, 70);
+            
+            // Paws
+            fill(255);
+            ellipse(x + 515, y + 680, 14, 10);
+            ellipse(x + 485, y + 680, 14, 10);
+
+
+            // Ears
+            fill(255, 200, 210);
+            ellipse(x + 485, y + 560, 16, 60);
+            ellipse(x + 515, y + 560, 16, 60);
+            
+            // Head
+            fill(255, 255, 255); 
+            circle(x + 500, y + 600, 65);
+            
+            //  eyes
+            fill(0); 
+            circle(x + 485, y + 595, 6); 
+            circle(x + 515, y + 595, 6);
+            
+            // nose
+            fill(255, 150, 160); 
+            circle(x + 500, y + 602, 5);
+
+            // Mouth
+            noFill();
+            strokeWeight(2);
+            arc(x + 500, y + 604, 10, 8, 0, PI);
+
+
+            // Arms
+            fill(255);
+            ellipse(x + 475, y + 655, 16, 12);
+            ellipse(x + 525, y + 655, 16, 12);
+        }
+    }
+
+
+
+    public void drawPubertyBunny() {
+        if (bunnyStage.equals("Puberty")) {
+            // Tail & Body
+            fill(255, 255, 255); 
+            stroke(0); 
+            strokeWeight(2);
+            circle(x + 435, y + 620, 26);
+            ellipse(x + 500, y + 610, 105, 100);
+            
+            // Ears
+            fill(255, 200, 210);
+            ellipse(x + 480, y + 490, 20, 80);
+            ellipse(x + 520, y + 490, 20, 80);
+            
+            // Head
+            fill(255, 255, 255);
+            circle(x + 500, y + 540, 85);
+            
+            // eyes
+            fill(0); 
+            circle(x + 480, y + 535, 8); 
+            circle(x + 520, y + 535, 8);
+            
+            // nose
+            fill(255, 150, 160); 
+            circle(x + 500, y + 542, 6);
+
+            // Whiskers
+            stroke(100); 
+            strokeWeight(1);
+           
+            // Left whiskers
+            line(x + 465, y + 546, x + 440, y + 544);
+            line(x + 465, y + 550, x + 438, y + 552);
+
+            // Right whiskers
+            line(x + 535, y + 546, x + 560, y + 544);
+            line(x + 535, y + 550, x + 562, y + 552);
+
+            // arms
+            fill(255);
+            ellipse(x + 460, y + 620, 16, 25);
+            ellipse(x + 540, y + 620, 16, 25);
+
+            // mouth
+            noFill();
+            strokeWeight(2);
+            arc(x + 496, y + 546, 9, 8, 0, HALF_PI + QUARTER_PI);
+            arc(x + 504, y + 546, 9, 8, HALF_PI, PI);
+            
+
+            // legs
+            fill(255);
+            ellipse(x + 465, y + 658, 24, 14); // Left leg
+            ellipse(x + 535, y + 658, 24, 14); // Right leg
+        }
+    }
+
+
+    public void drawTeenBunny() {
+        if (bunnyStage.equals("Teen")) {
+            // Tail and body
+            fill(255, 255, 255); 
+            stroke(0); 
+            strokeWeight(2);
+            circle(x + 420, y + 580, 32);
+            ellipse(x + 500, y + 570, 130, 130);
+            
+
+            // Arms
+            fill(255);
+            ellipse(x + 450, y + 580, 26, 18);
+            ellipse(x + 550, y + 580, 26, 18);
+
+
+            // Headphones
+            stroke(255, 50, 50);
+            strokeWeight(6);
+            noFill();
+            arc(x + 500, y + 520, 115, 60, 0, PI);
+
+            fill(50);
+            noStroke();
+            ellipse(x + 440, y + 525, 20, 30);
+            ellipse(x + 560, y + 525, 20, 30);
+            stroke(0);
+
+
+            // Ears
+            fill(255, 200, 210);
+            ellipse(x + 475, y + 410, 24, 110);
+            ellipse(x + 525, y + 410, 24, 110);
+            
+            // Head
+            fill(255, 255, 255); 
+            circle(x + 500, y + 480, 105);
+            
+            // eyes
+            fill(0); 
+            circle(x + 475, y + 475, 9); 
+            circle(x + 525, y + 475, 9);
+            
+            // nose
+            fill(255, 150, 160); 
+            circle(x + 500, y + 484, 8);
+
+
+            // legs
+            ellipse(x + 455, y + 630, 30, 18); // Left leg
+            ellipse(x + 545, y + 630, 30, 18); // Right leg
+
+
+            // mouth
+            noFill();
+            arc(x + 495, y + 491, 11, 9, 0, HALF_PI + QUARTER_PI);
+            arc(x + 505, y + 491, 11, 9, HALF_PI, PI);
+
+
+            // whiskers
+            stroke(50);
+            strokeWeight(1.2f);
+            // Left whiskers
+            line(x + 468, y + 488, x + 420, y + 482);
+            line(x + 468, y + 493, x + 415, y + 493);
+            line(x + 468, y + 498, x + 422, y + 504);
+            // Right whiskers
+            line(x + 532, y + 488, x + 580, y + 482);
+            line(x + 532, y + 493, x + 585, y + 493);
+            line(x + 532, y + 498, x + 578, y + 504);
+        }
+    }
+
+
+    public void drawAdultBunny() {
+        if (bunnyStage.equals("Adult")) {
+            // Tail & Body
+            fill(255, 255, 255); 
+            stroke(0); 
+            strokeWeight(2);
+            circle(x + 400, y + 540, 40);
+            ellipse(x + 500, y + 520, 165, 160);
+            
+            // Ears
+            fill(255, 200, 210);
+            ellipse(x + 470, y + 320, 30, 140);
+            ellipse(x + 530, y + 320, 30, 140);
+            
+            // Head
+            fill(255, 255, 255);
+            circle(x + 500, y + 410, 130);
+            
+            // eyes
+            fill(0); 
+            circle(x + 470, y + 400, 11); 
+            circle(x + 530, y + 400, 11);
+            
+            // nose
+            fill(255, 150, 160); 
+            circle(x + 500, y + 415, 10);
+
+
+            // Legs
+            fill(255);
+            ellipse(x + 445, y + 595, 38, 24); // Left leg
+            ellipse(x + 555, y + 595, 38, 24); // Right leg
+
+
+            // Arms
+            fill(255);
+            ellipse(x + 430, y + 530, 24, 38);
+            ellipse(x + 570, y + 530, 24, 38);
+
+
+            // whiskers
+            stroke(30);
+            strokeWeight(1.8f);
+
+            // Left whiskers
+            line(x + 460, y + 420, x + 380, y + 410);
+            line(x + 460, y + 425, x + 375, y + 425);
+            line(x + 460, y + 430, x + 385, y + 440);
+
+            // Right whiskers
+            line(x + 540, y + 420, x + 620, y + 410);
+            line(x + 540, y + 425, x + 625, y + 425);
+            line(x + 540, y + 430, x + 615, y + 440);
+
+            // mouth
+            noFill();
+            strokeWeight(2.2f);
+            arc(x + 494, y + 424, 12, 10, 0, HALF_PI + QUARTER_PI);
+            arc(x + 506, y + 424, 12, 10, HALF_PI, PI);
+        }
+    }
 
     public void mousePressed() {
         clicks++;
 
         if (clicks < childStage) {
            bunnyStage = "Baby";
-           bunnySize = stageIncrease[0]; // Multiplier 1
         }
-        
         else if (clicks < pubertyStage) {
             bunnyStage = "Child";
-            bunnySize = stageIncrease[1]; // Multiplier 2
         }
-
         else if (clicks < teenStage) {
             bunnyStage = "Puberty";
-            bunnySize = stageIncrease[2]; // Multiplier 3
         }
         else if (clicks < adultStage) {
             bunnyStage = "Teen";
-            bunnySize = stageIncrease[3]; // Multiplier 4
         }
-
-        else if (clicks >= adultStage && clicks <= 120) {
+        else if (clicks <= 120) {
             bunnyStage = "Adult";
-            bunnySize = stageIncrease[4]; // Multiplier 5
-        
         }
         else {
             clicks = 0;
             bunnyStage = "Baby";
-            bunnySize = stageIncrease[0]; // Reset Back to 1
         }
-
-
     }
-
-
-
-    
-
-    /** Additional helper methods below */
-
-}
+} 
